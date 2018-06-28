d1e475373ff1efa1969b79a665e634db7181272e
tanvirstreame <streametanvir@gmail.com>
2018-06-28 15:59:34 +0600

    Web

---
+++ Shop.Web/Areas/Buyer/BuyerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Shop.Web.Areas.Buyer
+{
+    public class BuyerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Buyer";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Buyer_default",
+                "Buyer/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
