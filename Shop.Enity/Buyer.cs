4ca439d15a2dd78abbbb9f87a6d922935cbb0d26
tanvirstreame <streametanvir@gmail.com>
2018-06-28 15:55:33 +0600

    Add project files.

---
+++ Shop.Enity/Buyer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Enity
+{
+    class Buyer
+    { public int Id { set; get; }   
+        public String Name { get; set; }
+        public String Address { get; set; }
+    }
+}
